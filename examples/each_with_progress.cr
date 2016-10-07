93e8c07dadb6d6924143451b3eb86aff8b9330e5
Andrea Manzini <ilmanzo@gmail.com>
2016-10-07 12:46:20 +0200

    formatted src

--- examples/each_with_progress.cr
+++ examples/each_with_progress.cr
@@ -1,18 +1,17 @@
 require "../src/progress"
 
 module WithProgress
-def each_with_progress(&block)
-  bar = ProgressBar.new
-  bar.total=self.size
-  bar.incomplete="."
-  bar.complete="o"
-  self.each do |i|
-    yield i
-    bar.inc
+  def each_with_progress(&block)
+    bar = ProgressBar.new
+    bar.total = self.size
+    bar.incomplete = "."
+    bar.complete = "o"
+    self.each do |i|
+      yield i
+      bar.inc
+    end
   end
 end
-end
-
 
 struct Range
   include WithProgress
@@ -22,16 +21,14 @@ class Array
   include WithProgress
 end
 
-
 puts "Range Demo:"
 (1...20).each_with_progress do |n|
   sleep 0.5
 end
 
 puts "Array Demo:"
-numbers=[4,8,15,16,23,42]
+numbers = [4, 8, 15, 16, 23, 42]
 
 numbers.each_with_progress do |nr|
   sleep 0.5
 end
-
